2cbb1c3db6c181c81cc8644ee1a1cdcda0fe04dd
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-08-28 14:49:44 -0700

    Changing int to Integer

--- api/src/main/java/ca/bc/gov/educ/api/pen/trax/repository/ProvExamRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/pen/trax/repository/ProvExamRepository.java
@@ -10,5 +10,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ProvExamRepository extends JpaRepository<ProvExamEntity, ProvExamId> {
-  int countAllByProvExamIdStudNo(String studNo);
+  Integer countAllByProvExamIdStudNo(String studNo);
 }
